39dbda5a0d280b70a8442c6c4a7b4aa58450a23b
gordob <34238250+gord0b@users.noreply.github.com>
2020-07-05 15:26:48 +0200

    added RapydBlok.com

--- ad/process.js
+++ ad/process.js
@@ -9,6 +9,16 @@ $(document).ready(function () {
         digits: '8',
         amountImpressions: '50'
     }
+    {
+        index: 1,
+        website: 'RapydBlok.com',
+        address: '3Jhhq39fSg6y44EDSf8xprHGWaKfdphBjbR',
+        currency: 'TN',
+        price: '100000000',
+        assetId: 'TN',
+        digits: '8',
+        amountImpressions: '25'
+    }
     ];
     var totalMessageString ="";
     var totalPriceInt=0;
